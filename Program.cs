fe34a41622d7f88e7599349daf802bd7fabbba6b
Gumani Ratshisuka <62070830+Gumie22@users.noreply.github.com>
2020-10-30 16:04:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//This is a C# program using the while loop to index the number given
+using System;
+namespace Indexing.cs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int index = 1;
+            while (index <= 5)
+            { 
+                Console.WriteLine(index);
+                index++;
+            }
+        }
+    }
+}
